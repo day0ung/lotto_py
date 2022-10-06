e7cd99909bd60ccee9dac76b43078b899f08530a
day0ung <jdy9436@naver.com>
2022-10-06 22:21:23 +0900

    db 연결부분 설정

--- lotto.py
+++ lotto.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup  #need pip install beautifulSoup4
+import pymysql  # pip install PyMySql
+
 import os
 
 
@@ -11,6 +13,7 @@ def getData():
 def main():
     his_data = getData()
     soup = BeautifulSoup(his_data, "html.parser")
+    conn = pymysql.connect(host='', port=3306, user='', password='', db='')
 
 
 if __name__ == "__main__":
